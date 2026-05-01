3b9565a61860fd174e3a67bb04a3430e7dff234c
Leo Mak <leomak613@gmail.com>
2026-05-01 01:35:03 -0700

    included a test app to demonstrate database

---
+++ testApp.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+    get_customers,
+    get_customer,
+    create_customer,
+} from "./database/database.js";
+
+const app = express();
+
+const PORT = 5000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/customers", async (req, res) => {
+    const customers = await get_customers();
+
+    res.render("customers", { customers });
+});
+
+app.listen(PORT, () => {
+    console.log(`Running Express server on port ${PORT}...`);
+});
