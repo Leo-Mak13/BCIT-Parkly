4410969b67e317a0aff7bbdbac33390630bdd9cf
Leo Mak <leomak613@gmail.com>
2026-05-06 04:14:39 -0700

    changed a little formatting

--- database/schema.sql
+++ database/schema.sql
@@ -16,90 +16,83 @@ DROP TABLE IF EXISTS `parking_lots`;
 
 DROP TABLE IF EXISTS `customers`;
 
-CREATE TABLE
-    `customers` (
-        customer_id INT PRIMARY KEY AUTO_INCREMENT,
-        customer_name VARCHAR(50) NOT NULL,
-        email VARCHAR(50) NOT NULL,
-        phone VARCHAR(10),
-        valid_permits VARCHAR(20) NOT NULL CHECK (valid_permits IN ('staff', 'student', 'public'))
-    );
+CREATE TABLE `customers` (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(10),
+    valid_permits VARCHAR(20) NOT NULL CHECK (valid_permits IN ('staff', 'student', 'public'))
+);
 
-CREATE TABLE
-    `parking_lots` (
-        lot_id INT PRIMARY KEY AUTO_INCREMENT,
-        lot_floor VARCHAR(10) NOT NULL UNIQUE,
-        lot_type TEXT NOT NULL CHECK (lot_type IN ('staff', 'student', 'public')),
-        lot_capacity INT NOT NULL,
-        street VARCHAR(100) NOT NULL,
-        city VARCHAR(50) NOT NULL,
-        province VARCHAR(50) NOT NULL,
-        postal_code VARCHAR(10) NOT NULL,
-        lot_availability TEXT CHECK (
-            lot_availability IN ('available', 'full', 'closed')
-        ),
-        lat DECIMAL(10, 7),
-        lon DECIMAL(10, 7)
-    );
+CREATE TABLE `parking_lots` (
+    lot_id INT PRIMARY KEY AUTO_INCREMENT,
+    lot_floor VARCHAR(10) NOT NULL UNIQUE,
+    lot_type TEXT NOT NULL CHECK (lot_type IN ('staff', 'student', 'public')),
+    lot_capacity INT NOT NULL,
+    street VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    province VARCHAR(50) NOT NULL,
+    postal_code VARCHAR(10) NOT NULL,
+    lot_availability TEXT CHECK (
+        lot_availability IN ('available', 'full', 'closed')
+    ),
+    lat DECIMAL(10, 7),
+    lon DECIMAL(10, 7)
+);
 
-CREATE TABLE
-    `parking_lot_schedules` (
-        schedule_id INT PRIMARY KEY AUTO_INCREMENT,
-        lot_id INT NOT NULL,
-        start_time TIME,
-        end_time TIME,
-        price DECIMAL(10, 2) NOT NULL,
-        FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE
-    );
+CREATE TABLE `parking_lot_schedules` (
+    schedule_id INT PRIMARY KEY AUTO_INCREMENT,
+    lot_id INT NOT NULL,
+    start_time TIME,
+    end_time TIME,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-CREATE TABLE
-    `parking_lot_valid_permits` (
-        lot_id INT NOT NULL,
-        valid_permits VARCHAR(20) NOT NULL CHECK (valid_permits IN ('staff', 'student', 'public')),
-        PRIMARY KEY (lot_id, valid_permits),
-        FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE
-    );
+CREATE TABLE `parking_lot_valid_permits` (
+    lot_id INT NOT NULL,
+    valid_permits VARCHAR(20) NOT NULL CHECK (valid_permits IN ('staff', 'student', 'public')),
+    PRIMARY KEY (lot_id, valid_permits),
+    FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-CREATE TABLE
-    `parking_stalls` (
-        stall_id INT PRIMARY KEY AUTO_INCREMENT,
-        occupied BOOLEAN DEFAULT FALSE,
-        parking_type TEXT NOT NULL CHECK (
-            parking_type IN ('regular', 'electric', 'small', 'handicap')
-        ),
-        centroid_lat DECIMAL(10, 7),
-        centroid_lng DECIMAL(10, 7),
-        polygon_wkt TEXT,
-        coordinates_json JSON,
-        POLYGON GEOMETRY SRID 4326 NULL,
-        lot_id INT,
-        FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE
-    );
+CREATE TABLE `parking_stalls` (
+    stall_id INT PRIMARY KEY AUTO_INCREMENT,
+    occupied BOOLEAN DEFAULT FALSE,
+    parking_type TEXT NOT NULL CHECK (
+        parking_type IN ('regular', 'electric', 'small', 'handicap')
+    ),
+    centroid_lat DECIMAL(10, 7),
+    centroid_lng DECIMAL(10, 7),
+    polygon_wkt TEXT,
+    coordinates_json JSON,
+    POLYGON GEOMETRY SRID 4326 NULL,
+    lot_id INT,
+    FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-CREATE TABLE
-    `parking_stall_vertices` (
-        vertex_order INT NOT NULL,
-        lat DECIMAL(10, 7) NOT NULL,
-        lng DECIMAL(10, 7) NOT NULL,
-        stall_id INT NOT NULL,
-        PRIMARY KEY (stall_id, vertex_order),
-        FOREIGN KEY (stall_id) REFERENCES parking_stalls (stall_id) ON DELETE CASCADE ON UPDATE CASCADE
-    );
+CREATE TABLE `parking_stall_vertices` (
+    vertex_order INT NOT NULL,
+    lat DECIMAL(10, 7) NOT NULL,
+    lng DECIMAL(10, 7) NOT NULL,
+    stall_id INT NOT NULL,
+    PRIMARY KEY (stall_id, vertex_order),
+    FOREIGN KEY (stall_id) REFERENCES parking_stalls (stall_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-CREATE TABLE
-    `reservations` (
-        reservation_id INT PRIMARY KEY AUTO_INCREMENT,
-        purchase_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-        license_plate VARCHAR(10) NOT NULL UNIQUE,
-        total_cost DECIMAL(10, 2) NOT NULL,
-        stall_location VARCHAR(20) NOT NULL,
-        lot_id INT,
-        stall_id INT,
-        customer_id INT,
-        FOREIGN KEY (customer_id) REFERENCES customers (customer_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY (stall_id) REFERENCES parking_stalls (stall_id) ON DELETE CASCADE ON UPDATE CASCADE
-    );
+CREATE TABLE `reservations` (
+    reservation_id INT PRIMARY KEY AUTO_INCREMENT,
+    purchase_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    license_plate VARCHAR(10) NOT NULL UNIQUE,
+    total_cost DECIMAL(10, 2) NOT NULL,
+    stall_location VARCHAR(20) NOT NULL,
+    lot_id INT,
+    stall_id INT,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (lot_id) REFERENCES parking_lots (lot_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (stall_id) REFERENCES parking_stalls (stall_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 INSERT INTO
     customers (customer_name, email, phone, valid_permits)
