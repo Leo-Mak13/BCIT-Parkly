ed87e2a492ec7d82cad87b6c03bd2e0380c73886
dwhiteBCIT <dwhite170@my.bcit.ca>
2026-05-08 11:41:54 -0700

    improved current styling on myReservations page and created the page to view induvidual reservations

--- database.ts
+++ database.ts
@@ -1,13 +1,15 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
 import "dotenv/config";
 
-export const pool: any = mysql.createPool({
-  host: process.env.MYSQL_HOST || "localhost",
-  port: Number(process.env.MYSQL_PORT) || 2911,
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "bcit_parkly",
-});
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST || "localhost",
+    port: Number(process.env.MYSQL_PORT) || 2911,
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "",
+    database: process.env.MYSQL_DATABASE || "bcit_parkly",
+  })
+  .promise();
 
 // get ALL customers (returns an array of customer objects)
 async function get_customers() {
@@ -25,31 +27,6 @@ async function get_customer(id: number) {
     [id],
   );
 
-  return output;
-}
-
-async function get_reservations(id: string) {
-  const [output] = await pool.query(
-    `
-        SELECT reservation_id, stall_location, purchase_date, total_cost, license_plate FROM reservations
-        WHERE customer_id = ?
-    `,
-    [id],
-  );
-  return output;
-}
-
-async function get_reservation(id: string) {
-  const [output] = await pool.query(
-    `
-        select customer_id, stall_location, total_cost, purchase_date, parking_lots.lot_name, parking_lots.lot_floor, parking_stalls.parking_type, parking_lot_address.street, parking_lot_address.city, parking_lot_address.province FROM reservations
-        INNER JOIN parking_lots ON reservations.lot_id = parking_lots.lot_id
-        INNER JOIN parking_stalls ON reservations.stall_id = parking_stalls.stall_id
-        INNER JOIN parking_lot_address ON parking_lots.lot_id = parking_lot_address.lot_id
-        WHERE reservation_id = ?
-        `,
-    [id],
-  );
   return output;
 }
 
@@ -75,21 +52,32 @@ async function create_customer(
   };
 }
 
-//insert reservation into database
+async function get_reservations(id: string) {
+  const [output] = await pool.query(
+    `
+        SELECT stall_location, purchase_date FROM reservations
+        WHERE customer_id = ?
+    `,
+    [id],
+  );
+  return output;
+}
+
+async function get_reservation(id: string) {
+  const [output] = await pool.query(
+    `
+        select stall_location, total_cost, purchase_date FROM reservations
+        WHERE reservation_id = ?
+        `,
+    [id],
+  );
+  return output;
+}
+
 async function create_reservation(
   license_plate: string,
-  total_cost: number,
   stall_location: string,
-  lot_id: number,
-  stall_id: number,
-  customer_id: number,
-) {
-  const [result] = await pool.query(
-    `INSERT INTO reservations (license_plate, total_cost, stall_location, lot_id, stall_id, customer_id)
-        VALUES(?, ?, ?, ?, ?, ?)`,
-    [license_plate, total_cost, stall_location, lot_id, stall_id, customer_id],
-  );
-}
+) {}
 
 async function main() {
   try {
@@ -115,11 +103,11 @@ async function main() {
 }
 
 // main();
+
 export {
   get_customers,
   get_customer,
+  create_customer,
   get_reservations,
   get_reservation,
-  create_customer,
-  create_reservation,
 };
