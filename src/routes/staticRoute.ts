6c47a6dee440f9143ac736019ffd3cf9d99b75aa
Shirley <jzheng96@my.bcit.ca>
2026-05-12 12:52:52 -0700

    correctly added authValidation to every route - now every route has access to req.user

--- src/routes/staticRoute.ts
+++ src/routes/staticRoute.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { authValidation } from "../middleware/authMiddleware";
 const router = express.Router();
+
+router.use(authValidation);
 
 router.get("/permitsrates", async (req, res) => {
   res.render("permits-rates");
