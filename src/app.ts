50ce5a04106d87aa3b5e1abf0e2a28c4dfb2fe4a
Shirley <jzheng96@my.bcit.ca>
2026-05-12 14:28:10 -0700

    created static routes, controllers, and updated every page to have updated routes, also update prefixes in app.py

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,4 @@
 import express from "express";
-import {
-  get_customers,
-  get_customer,
-  create_customer,
-} from "../database/database.ts";
-import reserveRoute from "../src/routes/reserveRoute.js";
 import { EOL } from "os";
 import cookieParser from "cookie-parser";
 
@@ -16,6 +10,7 @@ app.use(cookieParser());
 import lotRoutes from "./routes/lotRoutes";
 import userRoute from "./routes/userRoute.ts";
 import staticRoute from "./routes/staticRoute.ts";
+import reserveRoute from "./routes/reserveRoute.ts";
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -24,13 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", lotRoutes);
-app.use("/reservations", reserveRoute);
-
-app.use("/", userRoute);
-
-app.use("/", staticRoute);
-app.use("/permitsrates", staticRoute);
-app.use("/helpsupport", staticRoute);
+app.use("/reserve", reserveRoute);
+app.use("/users", userRoute);
+app.use("/info", staticRoute);
 
 app.listen(PORT, () => {
   console.log(`Running Express server${EOL}http://localhost:5000`);
