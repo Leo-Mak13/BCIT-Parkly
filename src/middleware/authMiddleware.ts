7debf357101dfb1f02ce347c9db7811cfaa2fea9
shirlsss <shirleyzheng2001@hotmail.com>
2026-05-13 22:34:00 -0700

    added docstrings for user/auth/login functions/services, created ensureValidated middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -4,7 +4,7 @@ import { validateSessionToken } from "../services/authService";
 import { Request, Response, NextFunction } from "express";
 import { getUserById } from "../services/userService";
 
-/*
+/**
  *@func authentication middleware - call next(), after checking session validation
  *@params req, res, nextFunction
  *@returns populates a req.user with either null (no associated session, so no user) or a populated req.user (session validated, attached corresponding id and email from users table)
@@ -41,10 +41,38 @@ export async function authValidation(
   next();
 }
 
+/**
+ * @func ensureValidated - ONLY go to next function IF session exists AND validated
+ * @param req
+ * @param res
+ * @param next - next function
+ */
 export async function ensureValidated(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
   const token = req.cookies.auth_session;
+  if (!token) {
+    res.redirect("/");
+  }
+  // if token, ie. session exists
+
+  try {
+    const session = await validateSessionToken(token);
+    if (!session) {
+      // if session expired - then redirect to "/", NOT VALIDATED
+      res.clearCookie("auth_session");
+      req.user = null;
+      res.redirect("/");
+    } else {
+      const userId = session.user_id;
+      const user = await getUserById(userId);
+      req.user = { id: userId, email: user.email };
+      next();
+    }
+  } catch (err) {
+    req.user = null;
+    res.redirect("/");
+  }
 }
