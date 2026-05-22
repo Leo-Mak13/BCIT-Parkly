8f3ac2183ef9ba88c4831002f09b82b3354a194a
Leo Mak <leomak613@gmail.com>
2026-05-21 20:54:05 -0700

    added event to unoccupy stalls when no reservation during current time

--- database/events.sql
+++ database/events.sql
@@ -1,2 +1,16 @@
 USE `bcit_parkly`;
 
+-- automatically free stalls once their reservation end_time has passed
+    -- runs every minute and unoccupies stalls whose reservation has expired
+DELIMITER //
+CREATE EVENT `free_stalls_on_reservation_expiry`
+ON SCHEDULE EVERY 1 MINUTE
+DO
+BEGIN
+    UPDATE parking_stalls
+    SET occupied = FALSE
+    WHERE stall_id IN (
+        SELECT stall_id FROM reservations WHERE end_time < NOW()
+    );
+END //
+DELIMITER ;
